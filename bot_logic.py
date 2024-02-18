dc4b01a61bc9a672122c0440165bcd1707a0ec0f
AbbyGoose <157957828+AbbyGoose@users.noreply.github.com>
2024-02-18 14:09:12 +0300

    Update and rename bot_logic to bot_logic.py

---
+++ bot_logic.py
@@ -0,0 +1,41 @@
+import random
+import time
+import discord
+def coin():
+    num = random.randint(0, 1)
+    print('Бросаем монетку...')
+    time.sleep(2)
+    if num == 0:
+        return 'Орёл!'
+    else:
+        return 'Решка!'
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+    for i in range(pass_length):
+        password += random.choice(elements)
+    return password
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTIwNjE4ODI1MDMzMTM1NzIxNA.GB8F1I.KYyOOVcwWTyy2_uPjBCM0EL_O6549JekIQre14")
